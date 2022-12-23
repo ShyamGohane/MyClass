1f723a4dac02246510240403609fbf40a24cc9cd
Your Name <you@example.com>
2022-12-23 15:09:17 +0530

    create MyClass2 added code for set

---
+++ MyClass/src/com/MyClass/MyClass2.java
@@ -0,0 +1,12 @@
+package com.MyClass;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MyClass2 {
+	public static void main(String[] args) {
+		Set<Integer> s=new HashSet<>();
+		s.add(10);
+		s.add(30);
+	}
+
+}
